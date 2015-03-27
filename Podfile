8bd0f23816802affd2f82d386d71b8e51bbae650
Ilya Mikhaltsou <morpheby@gmail.com>
2015-03-27 20:40:56 +0300

    Initial

---
+++ Podfile
@@ -0,0 +1,8 @@
+use_frameworks!
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+
+
+
---
+++ virus-spread/virus-spread/virus-spread/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  virus-spread
+//
+//  Created by Илья Михальцов on 27.3.15.
+//  Copyright (c) 2015 morpheby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
