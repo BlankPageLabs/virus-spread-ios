360729683976adbe5d9f1c2b6a7ac21b0a724253
Ilya Mikhaltsou <morpheby@gmail.com>
2015-04-18 02:03:58 +0300

    Another mistake with iTunes Connect versioning
    Now builds should update automatically in TestFlight

--- virus-spread/versionGen.sh
+++ virus-spread/versionGen.sh
@@ -16,7 +16,7 @@ if [ -d "${GIT_DIR}" ]; then
 		GIT_COMMIT_COUNT=`xcrun git rev-list ${GIT_TAG}..HEAD | wc -l | tr -d ' '`
         GIT_TOTAL_COMMIT_COUNT=`xcrun git rev-list HEAD | wc -l | tr -d ' '`
         RELEASE_VERSION=`xcrun git describe`
-        BUNDLE_VERSION_SHORT="${GIT_TAG}.${GIT_COMMIT_COUNT}"
+        BUNDLE_VERSION_SHORT="${GIT_TAG}"
         BUNDLE_VERSION="${GIT_TOTAL_COMMIT_COUNT}"
 elif [ -d "${SVN_DIR}" ]; then
         BUNDLE_VERSION_SHORT=`xcrun svnversion -nc "${PROJECT_DIR}" | sed -e 's/^[^:]*://;s/[A-Za-z]//' | tr -d ' '`
@@ -31,7 +31,7 @@ if [ -z "$1" ]; then
 else
         echo "#define ${PREFIX}BUNDLE_VERSION ${BUNDLE_VERSION}" > $1
         echo "#define ${PREFIX}BUNDLE_VERSION_SHORT ${BUNDLE_VERSION_SHORT}" >> $1
-        echo "#define ${PREFIX}VERSION_STRING @\"${BUNDLE_VERSION_SHORT}\"" >> $1
+        echo "#define ${PREFIX}VERSION_STRING @\"${BUNDLE_VERSION_SHORT}.${GIT_COMMIT_COUNT}\"" >> $1
         echo "#define ${PREFIX}RELEASE_STRING @\"${RELEASE_VERSION}\"" >> $1
 
         find "${PROJECT_DIR}" -iname "*.plist" -maxdepth 2 -exec touch {} \;
