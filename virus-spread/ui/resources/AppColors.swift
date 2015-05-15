78a3510e7da65eb74b8cb2ddc8cc8df7870ccf7b
Ilya Mikhaltsou <morpheby@gmail.com>
2015-05-15 18:39:11 +0300

    Updated colors

--- virus-spread/ui/resources/AppColors.swift
+++ virus-spread/ui/resources/AppColors.swift
@@ -8,7 +8,36 @@
 
 import UIKit
 
-class AppColors: NSObject {
-    static let textFieldBorderColor: UIColor = UIColor.whiteColor()
-    static let backgroundColor: UIColor = UIColor(red: 51.0/255.0, green: 155.0/255.0, blue: 213.0/255.0, alpha: 1.0)
+private let whiteColor = UIColor.whiteColor()
+private let skyBlueColor = UIColor(red: 51.0/255.0, green: 155.0/255.0, blue: 213.0/255.0, alpha: 1.0)
+private let blueColor = UIColor(red: 0.0/255.0, green: 172.0/255.0, blue: 237.0/255.0, alpha: 1.0)
+private let grayColor = UIColor(red: 150.0/255.0, green: 192.0/255.0, blue: 214.0/255.0, alpha: 1.0)
+
+class AppColors {
+    struct TextField {
+        struct Colors {
+            let borderColor: UIColor
+            let backgroundColor: UIColor
+            let textColor: UIColor
+            let placeholderColor: UIColor = grayColor
+        }
+
+        let normal = Colors(
+            borderColor: whiteColor,
+            backgroundColor: skyBlueColor,
+            textColor: whiteColor
+        )
+
+        let active = Colors(
+            borderColor: whiteColor,
+            backgroundColor: whiteColor,
+            textColor: blueColor
+        )
+    }
+    static let textField = TextField()
+
+    struct Screen {
+        let backgroundColor = skyBlueColor
+    }
+    static let screen = Screen()
 }
