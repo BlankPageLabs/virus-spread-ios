87fea4b0cd8acf9cb14ce48d30797d5fecdaa3d5
Ilya Mikhaltsou <morpheby@gmail.com>
2015-05-16 00:54:27 +0300

    Added stilizedButton

---
+++ virus-spread/ui/views/StilizedButton/StilizedButton.swift
@@ -0,0 +1,50 @@
+//
+//  StilizedButton.swift
+//  virus-spread
+//
+//  Created by Илья Михальцов on 15.5.15.
+//  Copyright (c) 2015 morpheby. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class StilizedButton: UIButton {
+
+    func customize() {
+        self.setTitleColor(AppColors.button.normal.textColor, forState: .Normal)
+        self.setTitleColor(AppColors.button.normalHighlighted.textColor, forState: .Normal | .Highlighted)
+        self.setTitleColor(AppColors.button.disabled.textColor, forState: .Disabled)
+
+        self.layer.borderWidth = 1.0
+        self.layer.cornerRadius = 5.0
+
+        self.syncVisualState()
+    }
+
+    func syncVisualState() {
+        self.layer.borderColor = AppColors.button.colorForState(self.state).borderColor.CGColor
+        self.backgroundColor = AppColors.button.colorForState(self.state).backgroundColor
+    }
+
+    override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        self.syncVisualState()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.customize()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.customize()
+    }
+
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        super.touchesEnded(touches, withEvent: event)
+    }
+}
