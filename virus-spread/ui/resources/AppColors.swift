309d090c2891d9bee730c5f37978b0180b52a006
Ilya Mikhaltsou <morpheby@gmail.com>
2015-05-15 18:18:57 +0300

    Added AppColors

---
+++ virus-spread/ui/resources/AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  AppColors.swift
+//  virus-spread
+//
+//  Created by Илья Михальцов on 15.5.15.
+//  Copyright (c) 2015 morpheby. All rights reserved.
+//
+
+import UIKit
+
+class AppColors: NSObject {
+    static let textFieldBorderColor: UIColor = UIColor.whiteColor()
+    static let backgroundColor: UIColor = UIColor(red: 51.0/255.0, green: 155.0/255.0, blue: 213.0/255.0, alpha: 1.0)
+}
