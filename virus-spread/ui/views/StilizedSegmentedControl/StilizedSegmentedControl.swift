8da3580c67eb7114cf859fd7aebc3485b3059dd9
Ilya Mikhaltsou <morpheby@gmail.com>
2015-05-16 04:14:10 +0300

    Added stilizedSegmentedControl

---
+++ virus-spread/ui/views/StilizedSegmentedControl/StilizedSegmentedControl.swift
@@ -0,0 +1,39 @@
+//
+//  StilizedSegmentedControl.swift
+//  virus-spread
+//
+//  Created by Илья Михальцов on 16.5.15.
+//  Copyright (c) 2015 morpheby. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class StilizedSegmentedControl: UISegmentedControl {
+
+    func customize() {
+        self.setTitleTextAttributes([
+            NSFontAttributeName: UIFont(name: "OpenSans-Bold", size: 18.0)!
+            ], forState: .Normal)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.customize()
+    }
+
+    override init(items: [AnyObject]) {
+        super.init(items: items)
+
+        self.customize()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.customize()
+    }
+
+
+}
