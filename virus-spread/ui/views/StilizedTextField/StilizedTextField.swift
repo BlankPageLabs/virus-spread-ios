8542868f2d38b753391d4f4809de243843710fa0
Ilya Mikhaltsou <morpheby@gmail.com>
2015-05-16 03:12:27 +0300

    Fixed some font problems and invalid rect placing in StilizedTextField

--- virus-spread/ui/views/StilizedTextField/StilizedTextField.swift
+++ virus-spread/ui/views/StilizedTextField/StilizedTextField.swift
@@ -12,6 +12,8 @@ import UIKit
 class StilizedTextField: UITextField {
 
     func customize() {
+        self.placeholderFont = createPlaceholderFontWithFont(self.font)
+
         self.borderStyle = .None
         self.layer.borderWidth = 1.0
 
@@ -24,15 +26,33 @@ class StilizedTextField: UITextField {
         self.textColor = AppColors.textField.colorForState(self.state).textColor
     }
 
+    var placeholderFont: UIFont!
+
+    override dynamic var font: UIFont! {
+        didSet {
+            self.placeholderFont = createPlaceholderFontWithFont(font)
+            self.setNeedsLayout()
+        }
+    }
+
+    func createPlaceholderFontWithFont(font: UIFont) -> UIFont {
+        return UIFont(descriptor: font.fontDescriptor().fontDescriptorWithSymbolicTraits(.TraitItalic)!,
+            size: font.pointSize)
+    }
+
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return super.textRectForBounds(bounds).rectByInsetting(dx: 10.0, dy: (self.bounds.height - self.font.lineHeight)/2.0)
+    }
+
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return super.editingRectForBounds(bounds).rectByInsetting(dx: 10.0, dy: (self.bounds.height - self.font.lineHeight)/2.0)
+    }
+
     override func drawPlaceholderInRect(var rect: CGRect) {
         if let placeholder = self.placeholder {
-            let placeholderFont = UIFont(descriptor: self.font.fontDescriptor().fontDescriptorByAddingAttributes([UIFontDescriptorTraitsAttribute: [UIFontSymbolicTrait: Int(UIFontDescriptorSymbolicTraits.TraitItalic.rawValue)]]),
-                size: self.font.pointSize)
-            rect.inset(dx: 10.0, dy: (self.bounds.height - placeholderFont.lineHeight)/2.0)
-
             (placeholder as NSString).drawInRect(rect, withAttributes: [
                 NSForegroundColorAttributeName: AppColors.textField.colorForState(self.state).placeholderColor,
-                NSFontAttributeName: placeholderFont,
+                NSFontAttributeName: self.placeholderFont,
                 ])
         }
     }
